405fdedaeb5c3358524dfe7eb00e39a3a6efb60c
Raj Agarwal <rajagarwalgood@gmail.com>
2026-01-29 14:29:38 +0530

    Feature : Added movie model

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 const app = express(); //express app object
 import mongoose from "mongoose";
+// import { Movie } from "./models/movie.model.ts";
 
 //configuring incoming body request
 app.use(express.json());
@@ -17,6 +18,25 @@ app.listen(process.env.PORT, () => {
 
   mongoose
     .connect(process.env.DB_URL as string) //connects to the mongo server
-    .then(() => console.log("Connected!"))
+    .then(async () => {
+      console.log("Connected!");
+      // await Movie.create({
+      //   name: "Inception",
+      //   description:
+      //     "A skilled thief who steals secrets through dream-sharing technology is given a chance to erase his criminal past by planting an idea into a target's subconscious.",
+      //   casts: [
+      //     "Leonardo DiCaprio",
+      //     "Joseph Gordon-Levitt",
+      //     "Elliot Page",
+      //     "Tom Hardy",
+      //     "Ken Watanabe",
+      //   ],
+      //   trailerUrl: "https://www.youtube.com/watch?v=YoHD9XEInc0",
+      //   language: "English",
+      //   releaseDate: "2010-07-16",
+      //   director: "Christopher Nolan",
+      //   releaseStatus: "RELEASED",
+      // });
+    })
     .catch((err) => console.log(err));
 });
